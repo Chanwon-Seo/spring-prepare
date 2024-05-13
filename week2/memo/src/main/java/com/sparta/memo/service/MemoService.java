37b41b92b7dbdba35f3226d7326d317020d27735
Chanwon-Seo <scwonn60@gmail.com>
2024-05-14 07:30:55 +0900

    Query method

--- week2/memo/src/main/java/com/sparta/memo/service/MemoService.java
+++ week2/memo/src/main/java/com/sparta/memo/service/MemoService.java
@@ -29,7 +29,7 @@ public class MemoService {
 
     public List<MemoResponseDto> getMemos() {
         // DB 조회
-        return memoRepository.findAll().stream()
+        return memoRepository.findAllByOrderByModifiedAtDesc().stream()
                 .map(MemoResponseDto::new)
                 .toList();
     }
